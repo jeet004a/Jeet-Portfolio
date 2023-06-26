a84ff68d5f7236e07467a19768d8beb7d68a276a
wastijeet110@gmail.com <ClinkRatCheat>
2023-06-26 16:47:02 +0530

    First commit 123

--- Portjeet/settings.py
+++ Portjeet/settings.py
@@ -122,6 +122,9 @@ STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
 
+STATICFILES_DIRS = [BASE_DIR / "/app/static"] # new
+STATIC_ROOT = BASE_DIR / "staticfiles" # new
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
