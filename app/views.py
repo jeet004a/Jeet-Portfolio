521b0d59714ba0c224f2a219a17284eda6f1e942
wastijeet110@gmail.com <ClinkRatCheat>
2023-06-30 17:00:36 +0530

    fisrt commit of the day

--- app/views.py
+++ app/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render,HttpResponse
-
+from .models import Feedback
 # Create your views here.
 
 def index(request):
+    if request.method=="POST":
+        name=request.POST.get("name", "")
+        print("Name is "+name)
+        obj=Feedback(name=name)
+        obj.save()
     return render(request,'index.html')
